fb05c559631da80dde8ae9502783cd46e5425e9c
nivedV1 <chandrannived7@gmail.com>
2025-02-18 22:17:02 +0530

    a

---
+++ newtonfoeward.py
@@ -0,0 +1,8 @@
+from pylab import*
+x_val=[]
+y_val=[]
+n=int(input("Enter number of data points="))
+for i in range(n):
+    x_value.append(float(input("x data=")))
+    y_value.append(float(input("y data=")))
+print(x_value);print(y_value)
