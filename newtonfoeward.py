a2dc88e19ac0151877adb7bd005fc0189c312ae8
nivedV1 <chandrannived7@gmail.com>
2025-02-19 00:31:36 +0530

    diff table

--- newtonfoeward.py
+++ newtonfoeward.py
@@ -1,9 +1,30 @@
 from pylab import*
-x_val=[]
-y_val=[]
-n=int(input("Enter number of data points="))
-for i in range(n):
-    x_value.append(float(input("x data=")))
-    y_value.append(float(input("y data=")))
-print(x_value);print(y_value)
-hj
+#x_val=[]
+#y_val=[]
+#n=int(input("Enter number of data points="))
+#for i in range(n):
+#    x_val.append(float(input("x data=")))
+#    y_val.append(float(input("y data=")))
+def diff(a):
+    for i in range(len(a)-1):
+        print(i)
+        lst.append(a[i+1]-a[i])
+    return(lst)
+n=4
+x_val=[0,1,2,3]
+y_val=[1,0,1,10]
+
+print(x_val);print(y_val)
+#lst=[[] for _ in range(n-1)]
+lst=[]
+y=[[0]*i for i in range(n-1,0,-1)]
+print(y)
+for i in range(n-1):
+    y[0][i]=y_val[i+1]-y_val[i]
+for k in range(1,n-1):
+     for j in range(0,n-1-k):
+        y[k][j]=y[k-1][j+1]-y[k-1][j]               
+print(y)
+#k=1 j=0,1,2
+#k=2 j=0,1
+#k=3 j=0
