d111e26ea33959e49b0f75342c4399d9362ac9e4
nivedV1 <chandrannived7@gmail.com>
2025-02-24 15:31:28 +0530

    final

--- newtonfoeward.py
+++ newtonfoeward.py
@@ -1,19 +1,28 @@
 from pylab import*
-x_val=[]
-y_val=[]
-n=int(input("Enter number of data points="))
-for i in range(n):
-    x_val.append(float(input("x data=")))
-    y_val.append(float(input("y data=")))
+#x_val=[]
+#y_val=[]
+#n=int(input("Enter number of data points="))
+x_val=[1891,1901,1911,1921,1931]
+y_val=[46,66,81,93,101]
+n=5
+#for i in range(n):
+#    x_val.append(float(input("x data=")))
+#    y_val.append(float(input("y data=")))
 print(x_val);print(y_val)
-y=[[0]*i for i in range(n-1,0,-1)]
-print(y)
+z=[[0]*i for i in range(n-1,0,-1)]
+print(z)
 for i in range(n-1):   
-    y[0][i]=y_val[i+1]-y_val[i]
+    z[0][i]=y_val[i+1]-y_val[i]
 for k in range(1,n-1):
      for j in range(0,n-1-k):
-        y[k][j]=y[k-1][j+1]-y[k-1][j]               
+        z[k][j]=z[k-1][j+1]-z[k-1][j]               
+print(z)
+xi=float(input("enter x to find y="))
+p=((xi-x_val[0])/(x_val[1]-x_val[0]))
+print(p)
+a=1
+y=y_val[0]
+for i in range(n-1):
+    a=(a*(p-i))/(i+1)
+    y=y+a*z[i][0]
 print(y)
-for i in range(n):
-    inter=(y[i][1])
-print(inter)
