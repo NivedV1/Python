34904a52b3ac1cee0e442055c5f21d5dc45c24c0
nivedV1 <chandrannived7@gmail.com>
2025-02-25 20:03:46 +0530

    g

--- gauss_jordan.py
+++ gauss_jordan.py
@@ -7,14 +7,14 @@ a=3
 #        u[p][l]=b
 u=[[5,2,3,1,0,0],[6,5,6,0,1,0],[7,8,9,0,0,1]]
 u=array(u)
-print(u)
 while u[0][0]==0:
     for i in range(a):
         u[[0,i]]=u[[i,0]]
 print(u)
 for i in range(a):
-    for j in range(1,a):
-        c=(u[i][j])
-        u[i][j]=(u[i][j])
-        u[j][i]=u[j][i]-(u[0][i]*u[j][i])
+        c=(u[i][i])
+        print(c)
+    u[i][i]=(u[i][j-1])/c
+print(u)
+
 
