2639ab056cf73fae81def49724f369ddb71ad43d
nivedV1 <chandrannived7@gmail.com>
2025-03-03 14:57:35 +0530

    l;

--- gauss_jordan.py
+++ gauss_jordan.py
@@ -1,11 +1,11 @@
 from numpy import*
-a=3
-#u=zeros((a,a),float)
-#for p in range(a):
-#    for l in range(a):
-#        b=int(input("enter values="))
-#        u[p][l]=b
-u=[[5,2,4,1,0,0],[6,5,6,0,1,0],[7,8,9,0,0,1]]
+a=int(input("size of matrix="))
+u=zeros((a,2*a),float)
+for p in range(a):
+    for l in range(a):
+        b=int(input("enter values="))
+        u[p][l]=b
+        u[p][p+a]=1
 u=array(u,float)
 while u[0][0]==0:
     for i in range(a):
