69175fa7a812aa371565ce765e19306c64ab0349
nivedV1 <chandrannived7@gmail.com>
2025-03-18 15:16:24 +0530

    prime

--- prime_number.py
+++ prime_number.py
@@ -1,23 +1,13 @@
 def is_prime(n):
-    if n<=2:
+    if n<=1:
         return(False)
     if n==2:
         return(True)
-    if n==3:
-        return(True)
-    if n==5:
-        return(True)
-    if n%2 ==0:
-        return(False)
-    i=4
-    while i<=(n/2):
-        i=i+1
+    for i in range(2,int(n/2)+1):
         if n%i==0:
             return(False)
-        else:
-            return(True)
+    return(True)
 #numb=int(input("enter number to check prime="))
 #print(is_prime(numb))
-k=200
-for l in range(k):
+for l in range(0,100):
     print(l," ",is_prime(l))
