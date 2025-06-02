be6ecd68c56a8974256825a9419702cd910dce00
nivedV1 <chandrannived7@gmail.com>
2025-06-02 15:19:33 +0530

    45

--- quadratic.py
+++ quadratic.py
@@ -3,9 +3,13 @@ a=int(input("coeff of X^2="))
 b=int(input("coeff of X="))
 c=int(input("constant="))
 print("qudratic equation is",a,"*X^2+",b,"*X+",c)
-d=(-b+(((b**2)-(4*a*c))**0.5))/(2*a)
-e=(-b-(((b**2)-(4*a*c))**0.5))/(2*a)
-print(complex(round(d.real,4),d.imag))
-print(complex(round(e.real,4),e.imag))
+if a==0:
+    print("Not a quadratic equation")
+else:
+    d=(-b+(((b**2)-(4*a*c))**0.5))/(2*a)
+    e=(-b-(((b**2)-(4*a*c))**0.5))/(2*a)
+    print(complex(round(d.real,4),round(d.imag,4)))
+    print(complex(round(e.real,4),round(e.imag,4)))
+
 
     
