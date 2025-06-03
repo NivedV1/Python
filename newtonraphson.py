77896208412481c89687ee5fa27b4acc3a8b712e
nivedV1 <chandrannived7@gmail.com>
2025-06-03 14:40:26 +0530

    1

--- newtonraphson.py
+++ newtonraphson.py
@@ -1,1 +1,12 @@
-def 
+def f(x):
+    return x**4-x-10
+def df(x):
+    return 4*x**3-1
+def sol(x):
+    while True:
+        h=f(x)/df(x)
+        if abs(h)<0.000001:
+            break
+        x=x-h
+    return print(x)
+sol(2)
